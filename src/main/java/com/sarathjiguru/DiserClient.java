e148b9fa0f2d0f2975c7b3698ba4274474f4cdf7
sarathjiguru <sarath@peel.com>
2017-11-14 07:55:34 +0530

    sends commands and receives responses, manages connections

---
+++ src/main/java/com/sarathjiguru/DiserClient.java
@@ -0,0 +1,41 @@
+package com.sarathjiguru;/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+/**
+ * Sends one message when a connection is open and echoes back any received
+ * data to the server.  Simply put, the echo client initiates the ping-pong
+ * traffic between the echo client and server by sending the first message to
+ * the server.
+ */
+public final class DiserClient {
+
+    static final boolean SSL = System.getProperty("ssl") != null;
+    public static final String HOST = System.getProperty("host", "127.0.0.1");
+    public static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+    static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
+
+
+    public static void main(String[] args) throws Exception {
+        CommandExecutor cc = new CommandExecutor();
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+            System.out.println("Command set status:" + cc.write("SET$a$" + i));
+        }
+        cc.disconnect();
+
+    }
+}
