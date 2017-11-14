823f3b2cc89798de66472470a0dabf85f5074ea8
sarathjiguru <sarath@peel.com>
2017-11-14 21:44:37 +0530

    adding resource and api classes

---
+++ src/main/java/com/sarathjiguru/rest/api/SimpleTuple.java
@@ -0,0 +1,7 @@
+package com.sarathjiguru.rest.api;
+
+/**
+ * Created by sarath on 14/11/17.
+ */
+public class SimpleTuple {
+}
---
+++ src/test/java/com/sarathjiguru/rest/healthcheck/DiserHealthCheckTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by sarath on 14/11/17.
+ */
+public class DiserHealthCheckTest {
+
+}
