78bc0c52d49127673b677fb0e3bdb600e0fc5d24
sarathjiguru <sarath@peel.com>
2017-11-14 21:45:03 +0530

    adding java class with key, value

--- src/main/java/com/sarathjiguru/rest/api/SimpleTuple.java
+++ src/main/java/com/sarathjiguru/rest/api/SimpleTuple.java
@@ -4,4 +4,29 @@ package com.sarathjiguru.rest.api;
  * Created by sarath on 14/11/17.
  */
 public class SimpleTuple {
+
+    SimpleTuple() {
+
+    }
+
+    private String key;
+
+    private Object value;
+
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
 }
